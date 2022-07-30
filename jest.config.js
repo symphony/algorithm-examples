0a4ae99aee8640032a4efb4bfe839e845f68369a
symphony <jciurc@gmail.com>
2022-07-30 12:19:30 -0700

    update jest config to only search in compiled dist folder. all tests passing

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // cacheDirectory: "/tmp/jest_rs",
 
   // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
+  // clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
@@ -76,16 +76,16 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    // "mjs",
-    // "cjs",
-    // "jsx",
-    "ts",
-    // "tsx",
-    "json",
-    // "node"
-  ],
+  // moduleFileExtensions: [
+  //   "js",
+  //   // "mjs",
+  //   // "cjs",
+  //   // "jsx",
+  //   "ts",
+  //   // "tsx",
+  //   "json",
+  //   // "node"
+  // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -124,9 +124,9 @@ module.exports = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "./dist"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
