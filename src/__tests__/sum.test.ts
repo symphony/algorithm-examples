9cdaa513e8450244ca4ff9bf1cc0063e22c73a53
symphony <jciurc@gmail.com>
2022-07-31 00:05:25 -0700

    add more sum test examples

--- src/__tests__/sum.test.ts
+++ src/__tests__/sum.test.ts
@@ -5,15 +5,23 @@ describe('arrayContainsSum', () => {
     expect(typeof arrayContainsSum).toBe('function');
   });
 
-  it('should find the item and return its index', () => {
+  it('should find a matching sum in the array', () => {
+    expect(arrayContainsSum([0, 2, 4, 7, 9, 10], 0)).toBe(true);
     expect(arrayContainsSum([0, 2, 4, 7, 9, 10], 6)).toBe(true);
+    expect(arrayContainsSum([0, 2, 4, 7, 9, 10], 20)).toBe(true);
   });
 
-  it('should return null if item is not found', () => {
-    expect(arrayContainsSum([0, 2, 4, 7, 9, 10], 15)).toBe(false);
+  it('should work with negative integers', () => {
+    expect(arrayContainsSum([-2, 2, 5], -4)).toBe(true);
+    expect(arrayContainsSum([-2, 2, 5], 3)).toBe(true);
+  });
+
+  it('should return false if matching sum is not found', () => {
+    expect(arrayContainsSum([0, 2, 4, 7, 9, 10], -2)).toBe(false);
+    expect(arrayContainsSum([0, 2, 4, 7, 9, 10], 1)).toBe(false);
+    expect(arrayContainsSum([-3, 0, 2, 4, 7, 9, 10], 15)).toBe(false);
+    expect(arrayContainsSum([0, 2, 4, 7, 9, 10], 200)).toBe(false);
   });
 
   // todo: complexity tests
 });
-
-
